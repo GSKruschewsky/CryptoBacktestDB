7b773ad40f91e455c2e7a7d04eacf8ae09c1b677
Diego Lima <diego.lima@estrelabet.com>
2023-10-25 18:23:39 -0300

    criando lógica para enviar os arquivos json para a s3

---
+++ exporterApp/src/index.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+const getFormattedCurrentDate = require('./helper/date');
+require('dotenv').config();
+
+function exportToS3(bucketName, jsonData){
+    AWS.config.update({
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+        region: process.env.REGION
+    });
+    
+    const s3 = new AWS.S3();
+    
+    const params = {
+        Bucket: bucketName,
+        Key: `${getFormattedCurrentDate()}.json`,
+        Body: Buffer.from(JSON.stringify(jsonData), 'utf-8'),
+        ContentType: 'application/json'
+    };
+    
+    s3.upload(params, (err, data) => {
+        if (err) {
+            console.error('Erro ao fazer o upload:', err);
+        } else {
+            console.log('Arquivo enviado com sucesso para:', data.Location);
+        }
+    });
+}
+
+exportToS3('crypto-backtest-db', {teste: 'deu certo'})
