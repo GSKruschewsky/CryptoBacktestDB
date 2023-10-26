9cf5f05b7fbf2d638b8d3d3592b79f5350ec050b
GSKruschewsky <luhkynaat@gmail.com>
2023-10-25 23:09:55 -0300

    Binance collector as a function

--- DataCollect/Binance/index.js
+++ DataCollect/Binance/index.js
@@ -6,52 +6,55 @@ const exportToS3 = require("../../exporterApp/src/index");
 const base = "BTC";
 const quote = "USDT";
 
-const market = (base+quote).toLowerCase();
+module.exports = function (base, quote) {
+  const market = (base+quote).toLowerCase();
 
-const ws_url = 'wss://stream.binance.com:9443/stream?streams='+market+'@trade/'+market+'@depth20';
-const ws = new WebSocket(ws_url);
+  const ws_url = 'wss://stream.binance.com:9443/stream?streams='+market+'@trade/'+market+'@depth20';
+  const ws = new WebSocket(ws_url);
 
-let trades = null;
+  let trades = null;
 
-ws.on('close', () => {
-  console.log('[!] WebSocket closed.');
-});
+  ws.on('close', () => {
+    console.log('[!] WebSocket closed.');
+  });
 
-ws.on('error', (err) => {
-  console.log('[E] WebSocket :',err);
-});
+  ws.on('error', (err) => {
+    console.log('[E] WebSocket :',err);
+  });
 
-ws.on('open', () => {
-  console.log('[!] WebSocket opened.');
-});
+  ws.on('open', () => {
+    console.log('[!] WebSocket opened.');
+  });
 
-ws.on('message', (msg) => {
-  msg = JSON.parse(msg);
+  ws.on('message', (msg) => {
+    msg = JSON.parse(msg);
 
-  if (msg.stream == market+'@trade') {
-    // New market trade.
-    if (trades) {
-      let { T: time, q: amount, p: price, m } = msg.data;
-      trades.push({ market: base+'/'+quote , time, side: (m ? 'sell' : 'buy'), amount, price });
-    }
-    return;
-  }
-
-  if (msg.stream == market+'@depth20') {
-    // New orderbook update.
-    if (trades) {
-      let { lastUpdateId, ...orderbook } = msg.data;
-      // console.log('\n'+JSON.stringify({ time: Date.now(), orderbook, trades }));
-      let time = Date.now();
-      let time_str = new Date(time - 60e3*60*3).toISOString().split('.')[0];
-      exportToS3("crypto-backtest-db", { time, orderbook, trades }, `Binance_${time_str}`);
+    if (msg.stream == market+'@trade') {
+      // New market trade.
+      if (trades) {
+        let { T: time, q: amount, p: price, m } = msg.data;
+        trades.push({ market: base+'/'+quote , time, side: (m ? 'sell' : 'buy'), amount, price });
+      }
+      return;
     }
 
-    trades = [];
-    
-    return;
-  }
+    if (msg.stream == market+'@depth20') {
+      // New orderbook update.
+      if (trades) {
+        let { lastUpdateId, ...orderbook } = msg.data;
+        // console.log('\n'+JSON.stringify({ time: Date.now(), orderbook, trades }));
+        let time = Date.now();
+        let time_str = new Date(time - 60e3*60*3).toISOString().split('.')[0];
+        exportToS3("crypto-backtest-db", { time, orderbook, trades }, `Binance_${base}-${quote}_${time_str}`);
+      }
 
-  console.log('WebSocket message:', msg);
-  process.exit();
-});
+      trades = [];
+      
+      return;
+    }
+
+    console.log('WebSocket unexpected message:', msg);
+    process.exit();
+  });
+};
+
