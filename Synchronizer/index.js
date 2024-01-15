59d675fd79c8a6aa70ceb7da8498d24b2d32b5f6
GSKruschewsky <luhkynaat@gmail.com>
2024-01-15 17:46:04 -0300

    Added 'first_upd_nonce_key' debug error to stop bot.

--- Synchronizer/index.js
+++ Synchronizer/index.js
@@ -884,43 +884,47 @@ class Synchronizer {
     if (this.is_lantecy_test) this.diff_latency.push(ws_recv_ts - upd.timestamp);
 
     if (upd.first_update_nonce) {
-      if (upd.first_update_nonce > this.orderbook.last_update_nonce + 1) {
-        const _at = 'apply_orderbook_upd:';
-        const _error = 'upd.first_update_nonce ('+upd.first_update_nonce+') > orderbook.last_update_nonce + 1 ('+(this.orderbook.last_update_nonce + 1)+').';
+      if (this.orderbook.received_first_update) {
+        if (upd.first_update_nonce > this.orderbook.last_update_nonce + 1) {
+          const _at = 'apply_orderbook_upd:';
+          const _error = 'upd.first_update_nonce ('+upd.first_update_nonce+') > orderbook.last_update_nonce + 1 ('+(this.orderbook.last_update_nonce + 1)+').';
 
-        if (_prom) {
-          _prom.reject({ At: _at, error: _error });
-        } else {
-          console.log('[E]',_at,_error,'\n\nEnding connection...');
-          process.exit();
+          if (_prom) {
+            _prom.reject({ At: _at, error: _error });
+          } else {
+            console.log('[E]',_at,_error,'\n\nEnding connection...');
+            process.exit();
+          }
+          return;
         }
-        return;
+        /*
+        if (upd.first_update_nonce != this.orderbook.last_update_nonce + 1) {
+          const _at = 'apply_orderbook_upd:';
+          const _error = 'upd.first_update_nonce ('+upd.first_update_nonce+') != orderbook.last_update_nonce + 1 ('+(this.orderbook.last_update_nonce + 1)+').';
+
+          if (_prom) {
+            _prom.reject({ At: _at, error: _error });
+          } else {
+            console.log('[E]',_at,_error,'\n\nEnding connection...');
+            process.exit();
+          }
+          return;
+        }
+      } else {
+        if (upd.first_update_nonce > this.orderbook.last_update_nonce + 1) {
+          const _at = 'apply_orderbook_upd:';
+          const _error = 'upd.first_update_nonce ('+upd.first_update_nonce+') > orderbook.last_update_nonce + 1 ('+(this.orderbook.last_update_nonce + 1)+').';
+
+          if (_prom) {
+            _prom.reject({ At: _at, error: _error });
+          } else {
+            console.log('[E]',_at,_error,'\n\nEnding connection...');
+            process.exit();
+          }
+          return;
+        }
       }
-      // if (this.orderbook.received_first_update) {
-      //   if (upd.first_update_nonce <= this.orderbook.last_update_nonce + 1) {
-      //     const _at = 'apply_orderbook_upd:';
-      //     const _error = 'upd.first_update_nonce ('+upd.first_update_nonce+') <= orderbook.last_update_nonce + 1 ('+(this.orderbook.last_update_nonce + 1)+').';
-      //     if (_prom) {
-      //       _prom.reject({ At: _at, error: _error });
-      //     } else {
-      //       console.log('[E]',_at,_error,'\n\nEnding connection...');
-      //       process.exit();
-      //     }
-      //     return;
-      //   }
-      // } else {
-      //   if (upd.first_update_nonce > this.orderbook.last_update_nonce + 1) {
-      //     const _at = 'apply_orderbook_upd:';
-      //     const _error = 'upd.first_update_nonce ('+upd.first_update_nonce+') > orderbook.last_update_nonce + 1 ('+(this.orderbook.last_update_nonce + 1)+').';
-      //     if (_prom) {
-      //       _prom.reject({ At: _at, error: _error });
-      //     } else {
-      //       console.log('[E]',_at,_error,'\n\nEnding connection...');
-      //       process.exit();
-      //     }
-      //     return;
-      //   }
-      // }
+      */
     }
 
     // Updates 'delayed_orderbook' if its the case.
